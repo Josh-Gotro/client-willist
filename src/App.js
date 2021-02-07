82d4d6c2bdd907413fdfdce40fce8ab4130ef8e1
Josh-Gotro <joshuagauthreaux@gmail.com>
2021-02-07 13:06:44 -0600

    reintroduced useState and event listener

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-// import React, { useState } from 'react'
-import React from 'react';
+import React, { useState } from 'react'
 import './App.css';
 import Title from './Title.js'
 import LetterBank from './LetterBank.js'
 
 
 function App() {
-  // const [letters, setLetters] = useState([]);
+  const [letters, setLetters] = useState([]);
 
-  document.addEventListener(
-    "keydown", 
-    guessLetter
-  ) 
 
-  function guessLetter(e) {
+  document.addEventListener('keydown', (e) => {
     console.log(e.key)
-  //   setLetters(p => p.push(e))
-  }
+    setLetters(p => [...p, e])
+  });
+
+  // document.addEventListener(
+  //   "keydown", 
+  //   guessLetter
+  // ) 
+
+  // function guessLetter(e) {
+  //   console.log(e.key)
+  //   setLetters(p => [...p, e])
+  // }
 
   return (
     <>
